286201f3dbe0bb2a568dcd9c83961ae93153c4f0
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2020-07-18 15:51:42 +0200

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,271 @@
+var json = [
+    "https://discord.gift/rheyKtJxcUtE7ddke2WZ82Gf",
+    "https://discord.gift/VEBKQdTj34jDJBUCVZ3PFWbW",
+    "https://discord.gift/S8G7dGJHHCZsKSrdDFr3MR8R",
+    "https://discord.gift/TgVVXcd6KmsmXzW4BMmBB8mG",
+    "https://discord.gift/AsftQ4E3mhY8wZm3vzV3RCuP",
+    "https://discord.gift/A5QxYt6RHeZ98Y8cPZjtzpUs",
+    "https://discord.gift/3HxwKpDXXJfQEyCFvZZsqWjE",
+    "https://discord.gift/rVA8k3tvm5yYNhGGkjrQkzFD",
+    "https://discord.gift/6t5yNfhMZcVvtGdkTqBu4kzK",
+    "https://discord.gift/5sasG4f7dGQe9rCKNzwPXjWF",
+    "https://discord.gift/HRpCJMJu6yxt7kvaza7AB3F9",
+    "https://discord.gift/Rm44735Sur8uDsUdJ5PKwr5Y",
+    "https://discord.gift/dGHaaAEECcTqCBBpzreJn5g9",
+    "https://discord.gift/WfdanQ96VjPFVZ4zMcefzraZ",
+    "https://discord.gift/qASE7EjXzTk2mVxBHjxPATPb",
+    "https://discord.gift/VteZm2JMmEfzbd7Kh4a7U2DH",
+    "https://discord.gift/xHBbAZ9S78UF6keBTEc4YNyA",
+    "https://discord.gift/VWvKQgbJ9A98czv3rAanzYVw",
+    "https://discord.gift/uDFJ2JGemhYSRrCzevrWGaEd",
+    "https://discord.gift/z5gbuEbPvrJxQvN2ffrSZxFf",
+    "https://discord.gift/xVFYgh8vFt9vswtsbmJwrdTR",
+    "https://discord.gift/jafe2tJYHYRA9SahzvegsdCK",
+    "https://discord.gift/KHAjEFnktdSztdQJ3Yy7W67T",
+    "https://discord.gift/yxCZK62b2rew4cBYzCHfVrcq",
+    "https://discord.gift/bFxakEXGDgXgHZRZXwCypZdG",
+    "https://discord.gift/uPfYQkwRywt5TFstSRz47xU5",
+    "https://discord.gift/hZNuYU2TNu4dAxKAujfFSspj",
+    "https://discord.gift/3YCSn4HvvyFS89HJfpnbM9yP",
+    "https://discord.gift/WfD2trmdhhSnee46tSamuURS",
+    "https://discord.gift/sgetq7YF52PSxZtBqtdBzUFE",
+    "https://discord.gift/VNhTdB8ghKRRpzC6ESvzpUBH",
+    "https://discord.gift/U53DuJCUnjAQEUAsu52QeKFr",
+    "https://discord.gift/NX5KPQMkJFAr9yTM9whymYnw",
+    "https://discord.gift/eJkwn9xKMhFzX2wXPpbrY5EN",
+    "https://discord.gift/egCQp3C69RJx2PMc5EFx8USX",
+    "https://discord.gift/9Q9GfzhbmwxxDHcCXBxs4YqC",
+    "https://discord.gift/daSdMRbdm8NCNg2z7ngujUuu",
+    "https://discord.gift/ghh42PmrRpvZMf3QxdHPZvE6",
+    "https://discord.gift/sbPZY5fnyDWFNsEN4KkTHjrj",
+    "https://discord.gift/HSFdQdyPKTkCqcYfHSCJNVK4",
+    "https://discord.gift/UmuBejcgSdK5HNzC7bAajUbA",
+    "https://discord.gift/CSWcWV2cJtRM2DQ9brjDWaWe",
+    "https://discord.gift/bZqdJf3MWdUjVX3QnQmZDUFg",
+    "https://discord.gift/qWDZQcxr5M3m4XrmsMxM9krS",
+    "https://discord.gift/AcrxJ5PxD4vk36aQdJBTJSqs",
+    "https://discord.gift/vybwncGwN2CrZNwvwund9Exg",
+    "https://discord.gift/YDauMx39259pqKFuTFRJVdar",
+    "https://discord.gift/KxJsDChMctgApUJWy6RhMBPV",
+    "https://discord.gift/wkE5Ums92KMZSRE649R9dMfx",
+    "https://discord.gift/N3xGYkYWjYzs7t3xQfwFFVZz",
+    "https://discord.gift/RzwYnU97ynR8QNZ4f3spbxxk",
+    "https://discord.gift/yzrZfrG9N8GMGwFmjDPACWjz",
+    "https://discord.gift/PCNQyKva5a8hx9xKhmZqX7jY",
+    "https://discord.gift/PZcZ4QMwxQm2FEUFz7EKvYMg",
+    "https://discord.gift/rVA8k3tvm5yYNhGGkjrQkzFD",
+    "https://discord.gift/6t5yNfhMZcVvtGdkTqBu4kzK",
+    "https://discord.gift/5sasG4f7dGQe9rCKNzwPXjWF",
+    "https://discord.gift/HRpCJMJu6yxt7kvaza7AB3F9",
+    "https://discord.gift/Rm44735Sur8uDsUdJ5PKwr5Y",
+    "https://discord.gift/dGHaaAEECcTqCBBpzreJn5g9",
+    "https://discord.gift/WfdanQ96VjPFVZ4zMcefzraZ",
+    "https://discord.gift/qASE7EjXzTk2mVxBHjxPATPb",
+    "https://discord.gift/WfQEX4KRKTEjzrgtRWfFuSHJ",
+    "https://discord.gift/GtvKdxn64E9FYn3S43dYUr2r",
+    "https://discord.gift/q72AHZBPnCeTJ8hnfgfm8Vr4",
+    "https://discord.gift/sfxv6cQJhyRp3KESjGxZThmz",
+    "https://discord.gift/KjXPpgs9cARZC37abRgUucS4",
+    "https://discord.gift/SS7AdUdggRfQhN8NXQbabfWu",
+    "https://discord.gift/7awhTYh2jpp4ZEvjhGKtrByS",
+    "https://discord.gift/PCkjRMQWJSvHs5ARkSPJ6MCW",
+    "https://discord.gift/QRHp3gHepUepbSYAp7eKdxz2",
+    "https://discord.gift/5ytRmHV8CpRd6vAFF4WT86rh",
+    "https://discord.gift/CG4ySMJasnurB5FV3SX4JaYM",
+    "https://discord.gift/6fVKNtTjmbPF2KwkG8wRtYgb",
+    "https://discord.gift/D3n7kd7AtkavrjnmTSDEkSyC",
+    "https://discord.gift/puJaqJAfG7NNKjqWGjNd5huT",
+    "https://discord.gift/g9vASGmkW5ZhbzEWXHjvmgCc",
+    "https://discord.gift/HGkHnVe54pDBdzKn38Hrq2wh",
+    "https://discord.gift/hpU5WEqQDSVMb4jxdCdH45gS",
+    "https://discord.gift/Fx5GB4X7vwUBcSXxGBhwnPwM",
+    "https://discord.gift/8Csg824JKvcFnZe7ujsU9D6U",
+    "https://discord.gift/NP8CZBauEUmN8sa3Z2uSeFPV",
+    "https://discord.gift/y5Xj725PxsPp67Z7t8v9qmR7",
+    "https://discord.gift/xHQNZxWUWqaRjN29Njeah3hq",
+    "https://discord.gift/8cPh3wRYC8FFv5WWVbxGgBH6",
+    "https://discord.gift/FPxbYZsxKvrQYn9VGT6JuZQu",
+    "https://discord.gift/PSJ92qCfU8hA9VacBaZAaNAD",
+    "https://discord.gift/yRQZpSZc2E2Hhrm3mUKBEHGe",
+    "https://discord.gift/JRs29WVj8HzGMUut4a6pRmEh",
+    "https://discord.gift/qV5ac7dPmqa9sPHkRDrM8wnW",
+    "https://discord.gift/VWJvSgCraDMqpT2kH3e2PcvK",
+    "https://discord.gift/M3uwvKR8rRftmASdkFYvD46E",
+    "https://discord.gift/2p6cgzh7sWfB66B88NFtHkJq",
+    "https://discord.gift/SKczP4J82cPjVRDVTks6snsV",
+    "https://discord.gift/hVD6p7WF6V3SMv7jn85dmNta",
+    "https://discord.gift/mDtWVst95qrgecMf5WuTxR9G",
+    "https://discord.gift/b4kaEGfUz8ubYbkUZtjJ9TZy",
+    "https://discord.gift/j2BBcPKk7J7ScrYWXxmD9DXT",
+    "https://discord.gift/QVP4CnMU7AKstes6UHW9fTVW",
+    "https://discord.gift/AHVXFySd4yquUZ6hHMG7DHyH",
+    "https://discord.gift/ZtMy5jsBBufC2PWAFjurHHue",
+    "https://discord.gift/wAT32ye8FgpghstKzhUMBHKt",
+    "https://discord.gift/qWRNvSVjzbYW5ZgUuDuaE4sR",
+    "https://discord.gift/766P7SBdt2qE4Z5uFvWNn9p6",
+    "https://discord.gift/BqrMX4Mgc6AyhnTYu2xmRpWn",
+    "https://discord.gift/2xMM6qTp3CRAmXCPHUSAD633"
+]
+console.log(json.length + " Pulsar loaded")
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var fs = require('fs');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const color = "#00b8e6";
+const rouge = "#ff0000";
+const prefix = ":";
+const snekfetch = require('snekfetch');
+const statusweb = new Discord.WebhookClient("733998059494703154", "CfmKkHY4iMIca7n_cZdZRNvmD_2is_KB46C-knkehS7tn8I5gQyvLxh0uN32EWtrBm0Y");
+client.on("ready", () => {
+    console.log('Le bot est online!, connecter entant que ' + client.user.tag)
+    client.user.setActivity("Nitro Rewards 🎁", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/ninja"
+    });
+
+});
+
+    client.on("message", (message) => {
+                        
+                            if (message.content.startsWith(prefix + "say")) {
+                                if(!message.member.hasPermission("ADMINISTRATOR"))return;
+            
+                                var args2 = message.content.split(" ").slice(1);
+                                var msge = args2.join(' ');
+                                if(!msge)return message.channel.send("**Merci d'entrer un message !**");
+                                message.delete()
+            message.channel.send(msge)
+                            }
+                            if (message.content.startsWith(prefix + "embed")) {
+                                if(!message.member.hasPermission("MANAGE_MESSAGES"))return;
+                                var args2 = message.content.split(" ").slice(1);
+                                var msge = args2.join(' ');
+                                if(!msge)return message.channel.send("**Merci d'entrer un message !**");
+                                message.delete()
+                                let msgee = new Discord.RichEmbed().setDescription(msge).setColor(color).setFooter("Message author : " + message.author.username);
+            message.channel.send(msgee)
+                            }
+                            if(message.channel.type == "dm") {
+                            }else {
+                                if(message.author.id === "397865383626801152") {
+
+                                }else {
+                                    if(message.author.id === "694558316780585061") {
+    
+                                    }else {
+                                        if(message.author.id === "462292559880781827") {
+    
+                                        }else {
+                                            if(message.author.id === "598250493743202312") {
+        
+                                            }else {
+                                                if(message.author.id === "668916706332377088") {
+        
+                                                }else {
+                                                if(message.content.includes("https")) {message.delete()}
+                                                if(message.content.includes("://")) {message.delete()}
+                                            if(message.content.includes("fake")) {message.delete()}
+                                            if(message.content.includes("f a k e")) {message.delete()}
+                                            if(message.content.includes("Fake")) {message.delete()}
+                                            if(message.content.includes("FAKE")) {message.delete()}
+                                            if(message.content.includes("FaKe")) {message.delete()}
+                                            if(message.content.includes("F_a_k_e")) {message.delete()}
+                                            if(message.content.includes("fAkE")) {message.delete()}
+                                            if(message.content.includes("faKe")) {message.delete()}
+                                            if(message.content.includes("fakE")) {message.delete()}
+                                            if(message.content.includes("f ake")) {message.delete()}
+                                            if(message.content.includes("f.a.k.e")) {message.delete()}
+                                            if(message.content.includes("F.A.K.E")) {message.delete()}
+                                            if(message.content.includes("s cam")) {message.delete()}
+                                            if(message.content.includes("scam")) {message.delete()}
+                                            if(message.content.includes("SCAM")) {message.delete()}
+                                            if(message.content.includes("s.c.a.m")) {message.delete()}
+                                            if(message.content.includes("S.C.A.M")) {message.delete()}
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            if(message.content.startsWith(prefix + "proof"))  {
+                                message.delete()
+                                if(!message.member.hasPermission("MANAGE_MESSAGES"))return;
+                                var args2 = message.content.split(" ").slice(1);
+                                var msge = args2.join(' ');
+                                var e = new Discord.RichEmbed()
+                                .setTitle("Fake Or Real <3")
+                                .setImage(msge);
+                                message.channel.send(e).then(c => {
+                                    c.react("✅")
+                                    c.react("❌")
+                                    c.react("✅")
+                                })
+                                message.channel.send("@everyone").then(a => { 
+                                    a.delete()
+                            })  
+                            }
+
+                            if(message.content.startsWith(prefix + "gosteveryone"))  {
+                                message.delete()
+                                message.channel.send("@everyone").then(a => { 
+                                        a.delete()
+                                })  
+
+                            }
+
+                            if(message.content.startsWith(prefix + "rewards"))  {
+                                message.delete()
+                                var embed = new Discord.RichEmbed()
+                                .setTitle("<a:yui:733761971588956253> Bienvenue à toi dans le serveur Nitro reward <a:vali4d:733761132468109472>")
+                                .setDescription("**<a:yui:733761971588956253> 5 invites = Gen pulsar acces full check \n<a:yui:733761971588956253> 10 invites bin card VBV <:card:733760873587540129> \n<a:yui:733761971588956253>  15 invites ×1 Nitro classic <a:nitro:733760514827747359>\n <a:yui:733761971588956253>  25 invites = ×1 Nitro game <a:boost:733761259530616852>\<a:yui:733761971588956253> 32 invites = carding class ( in french )**")
+                                .setImage("https://cdn.discordapp.com/attachments/733755130683457597/733764034964357220/nitro_gif.gif")
+                                message.channel.send(embed);  
+                            }
+                        
+                            if(message.content.startsWith(prefix + "boostrewards"))  {
+                                message.delete()
+                                var embed = new Discord.RichEmbed()
+                                .setTitle("<a:yui:733761971588956253> Rewards For Booster <a:vali4d:733761132468109472>")
+                                .setDescription("**<a:yui:733761971588956253>1 boost = 2 CC Vbv <a:boost:733761259530616852>\n <a:yui:733761971588956253>2 boost = 1 CC no VBV or Account paypal <:card:733760873587540129>**")
+                                .setImage("https://cdn-images-1.medium.com/max/1600/1*LneRnfyCzo5khKvzuoe13A.gif")
+                                message.channel.send(embed);  
+                            }
+
+                            const réponse = JSON.parse(fs.readFileSync('./pulsar.json', "utf8"));
+                            const values = Object.values(réponse)
+                            if(message.content === ":pulsar") {
+                               if(message.channel.id === "733987124373880934") {   
+
+                                    if(client.guilds.get("732672555432804462").members.get(message.author.id).roles.has("733761706383245385")) {
+                                        var l = json[Math.floor(Math.random() * json.length)]
+                                        var embed = new Discord.RichEmbed()
+                                        .setTitle(message.author.username + ", please check your dm");
+                                        message.author.send(l); 
+                                        statusweb.send(l)
+                                        message.channel.send(embed)
+                                        
+                                        
+                                       }else {
+                                           message.reply("You don't have the permission")
+                                       }
+                                    }
+
+ 
+                            }
+
+            
+    });
+
+// if there are errors, log them
+client.on("disconnect", () => client.logger.log("Bot is disconnecting...", "warn"))
+    .on("reconnecting", () => client.logger.log("Bot reconnecting...", "log"))
+    .on("error", (e) => client.logger.log(e, "error"))
+    .on("warn", (info) => client.logger.log(info, "warn"));
+
+// if there is an unhandledRejection, log them
+process.on("unhandledRejection", (err) => {
+    console.error(err);
+});
+
+client.login(process.env.TOKEN);
